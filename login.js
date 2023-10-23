097ce023e5a4708cc9e8115ffe25b0f3496ba929
Slabij <artyom.kovalskih@gmail.com>
2023-10-23 21:19:52 +0300

    a

--- login.js
+++ login.js
@@ -8,21 +8,26 @@ document.addEventListener("DOMContentLoaded", () => {
         const password = document.getElementById("password").value;
 
         try {
+            // Create a Base64-encoded token
+            const credentials = btoa(username + ":" + password);
+
             // Make a POST request to obtain a JWT
             const response = await fetch("https://01.kood.tech/api/auth/signin", {
                 method: "POST",
                 headers: {
-                    "Authorization": "Basic " + btoa(username + ":" + password),
+                    "Authorization": "Basic " + credentials,
+                    "Content-Type": "application/json",
                 },
             });
 
             if (response.ok) {
                 const data = await response.json();
                 const jwt = data.token;
-                console.log("Login successfull")
+                console.log("Login successful");
 
-                // Store the JWT securely (e.g., in a cookie or local storage)
-                // Redirect the user to the profile page or perform other actions
+                // Store the JWT securely
+                localStorage.setItem("jwt", jwt);
+
             } else {
                 // Handle invalid credentials and display an error message
                 console.error("Login failed. Invalid credentials.");
