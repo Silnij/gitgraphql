813f8dfe7b40087396b6ab03782750a084b6355b
Slabij <artyom.kovalskih@gmail.com>
2023-10-23 19:50:19 +0300

    start

---
+++ login.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.getElementById("login-form");
+
+    loginForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+        try {
+            // Make a POST request to obtain a JWT
+            const response = await fetch("https://01.kood.tech/api/auth/signin", {
+                method: "POST",
+                headers: {
+                    "Authorization": "Basic " + btoa(username + ":" + password),
+                },
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                const jwt = data.token;
+
+                // Store the JWT securely (e.g., in a cookie or local storage)
+                // Redirect the user to the profile page or perform other actions
+            } else {
+                // Handle invalid credentials and display an error message
+                console.error("Login failed. Invalid credentials.");
+            }
+        } catch (error) {
+            console.error("An error occurred:", error);
+        }
+    });
+});
