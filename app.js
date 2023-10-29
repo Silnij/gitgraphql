5e6e9a2f5e258fe4e167b63575666d9037269987
Slabij <artyom.kovalskih@gmail.com>
2023-10-29 22:18:22 +0200

    query

--- app.js
+++ app.js
@@ -31,14 +31,10 @@ makeGraphQLQuery(userQuery)
     function updateProfileWithData(userData) {
         const userNameElement = document.getElementById('user-name');
         const userXpElement = document.getElementById('user-xp');
-        const userGradesElement = document.getElementById('user-grades');
         const userAuditsElement = document.getElementById('user-audits');
-        const userSkillsElement = document.getElementById('user-skills');
 
         userNameElement.textContent = userData.login;
-        userXpElement.textContent = userData.xp;
-        userGradesElement.textContent = userData.grades;
-        userAuditsElement.textContent = userData.audits;
-        userSkillsElement.textContent = userData.skills;
+        userXpElement.textContent = userData.xps;
+        userAuditsElement.textContent = userData.audit;
     }
     
