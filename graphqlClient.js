c9e49a6bf2872dbd8ee0a796b60c7526e23cb20d
Slabij <artyom.kovalskih@gmail.com>
2023-10-29 20:32:16 +0200

    smth

--- graphqlClient.js
+++ graphqlClient.js
@@ -3,24 +3,33 @@ const API_URL = 'https://01.kood.tech/api/graphql-engine/v1/graphql';
 async function makeGraphQLQuery(query, variables = {}) {
     const jwt = localStorage.getItem('jwt'); 
 
-    const response = await fetch(API_URL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${jwt}`,
-        },
-        body: JSON.stringify({
-            query,
-            variables,
-        }),
-    });
+    try {
+        const response = await fetch(API_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${jwt}`,
+            },
+            body: JSON.stringify({
+                query,
+                variables,
+            }),
+        });
 
-    if (!response.ok) {
-        throw new Error('GraphQL query failed');
+        if (!response.ok) {
+            throw new Error(`GraphQL query failed with status ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        if (data.errors) {
+            throw new Error(`GraphQL query returned errors: ${JSON.stringify(data.errors)}`);
+        }
+
+        return data;
+    } catch (error) {
+        throw new Error(`GraphQL query encountered an error: ${error.message}`);
     }
-
-    const data = await response.json();
-    return data;
 }
 
 export { makeGraphQLQuery };
