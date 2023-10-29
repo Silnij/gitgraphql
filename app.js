f301c208ab1c3b29a25091356d06e1d2bbfad310
Slabij <artyom.kovalskih@gmail.com>
2023-10-29 21:54:42 +0200

    query

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import { makeGraphQLQuery } from  "./graphqlClient.js";
 
 const userQuery = `
-        {
             user {
             id
             login
@@ -9,7 +8,6 @@ const userQuery = `
             audits
             skills
         } 
-    }
 `;
 
 makeGraphQLQuery(userQuery)
