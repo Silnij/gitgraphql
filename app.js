ef700507510fef17d00f2e1c5ae0df6992e0e56f
Slabij <artyom.kovalskih@gmail.com>
2023-10-30 19:17:59 +0200

    svg

--- app.js
+++ app.js
@@ -23,8 +23,27 @@ makeGraphQLQuery(userQuery)
         const totalXp = xps.reduce ((sum, xp) => sum + xp.amount, 0);
         console.log("xp:", totalXp);
         console.log("auditRatio:", userData[0].auditRatio);
+
+        const svgXp = document.getElementById ('xpChart')
+        const chartWidth = 400;
+        const chartHeight = 200;
+        const barWidth = chartWidth / xps.length;
+
+        xps.forEach((xp, index) => {
+        const barHeight = (xp.amount / totalXp) * chartHeight;
+        const x = index * barWidth;
+        const y = chartHeight - barHeight;
+
+        const bar = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+        bar.setAttribute("x", x);
+        bar.setAttribute("y", y);
+        bar.setAttribute("width", barWidth);
+        bar.setAttribute("height", barHeight);
+        bar.setAttribute("fill", "blue");
+
+        svgXp.appendChild(bar);
     })
     .catch(error => {
         console.error('Failed to fetch user data:', error);
     });
-    
+    });
